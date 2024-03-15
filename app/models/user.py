0cf37eaf5baf514e3ccde88d91ac038f9b0b6c33
Hao Xu <haoxugt@gmail.com>
2024-03-15 14:17:27 -0700

    Update user model and seeds, add likes tableand seeds

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,15 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+likes = db.Table(
+    "likes",
+    db.Model.metadata,
+    db.Column("user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
+    db.Column("song_id", db.Integer, db.ForeignKey(add_prefix_for_prod("songs.id")), primary_key=True)
+)
+
+if environment == "production":
+    likes.schema = SCHEMA
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -12,7 +21,13 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+    song_in_user_rel = db.relationship("Song", back_populates="artist_in_song_rel")
+    album_in_user_rel = db.relationship("Album", back_populates="artist_in_album_rel")
+    likes_in_user_rel = db.relationship("Song", secondary="likes", back_populates="likes_in_song_rel")
 
     @property
     def password(self):
@@ -29,5 +44,7 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name
         }
