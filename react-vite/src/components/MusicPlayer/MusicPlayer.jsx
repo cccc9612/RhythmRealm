1901d4e909007f98dd694a01b00fc9ee32be8665
Hao Xu <haoxugt@gmail.com>
2024-03-18 01:57:23 -0700

    Add react-h5-audio-player package in nodeJS and add music player component in react, need to figure out how to control player

---
+++ react-vite/src/components/MusicPlayer/MusicPlayer.jsx
@@ -0,0 +1,25 @@
+import AudioPlayer from 'react-h5-audio-player';
+
+import 'react-h5-audio-player/lib/styles.css';
+
+import './MusicPlayer.css'
+
+function MusicPlayer() {
+  // const nextSong() {
+
+  // }
+
+  return <AudioPlayer
+    autoPlay
+    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+    onPlay={e => console.log("onPlay", e)}
+    showSkipControls={true}
+    showJumpControls={false}
+    // onClickNext={nextSong}
+    // style={{ width: '600px'}}
+    layout="stacked-reverse"
+  // other props here
+  />;
+}
+
+export default MusicPlayer;
--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -5,6 +5,7 @@ import { ModalProvider, Modal } from "../context/Modal";
 import { thunkAuthenticate } from "../redux/session";
 import { LuLibrary } from "react-icons/lu";
 import ProfileButton from "../components/Navigation/ProfileButton";
+import MusicPlayer from "../components/MusicPlayer";
 // import OpenModalMenuItem from "../components/Navigation/OpenModalMenuItem";
 import Navigation from "../components/Navigation/Navigation";
 
@@ -18,23 +19,31 @@ export default function Layout() {
   return (
     <>
       <ModalProvider>
-        <div className="page-container">
-          <div className="sidebar-container">
-            <Navigation />
-            <div className="library-container">
-              <div className="library-title">
-                <LuLibrary /> Your library
+        <div className="page-player-container">
+          <div className="page-container">
+            <div className="sidebar-container">
+              <Navigation />
+              <div className="library-container">
+                <div className="library-title">
+                  <LuLibrary /> Your library
+                </div>
+                <div className="playlist-list">
+                  playlsit
+                </div>
               </div>
-              <div className="playlist-list">
-                playlsit
+            </div>
+            <div className="song-album-list-container">
+              <div className="title-bar">
+                <ProfileButton />
+              </div>
+              <div> {isLoaded && <Outlet />}</div>
+              <div className="footer-container">
+                <p>@ 2024 This site is for studying · <a href='https://github.com/cccc9612/RhythmRealm' target='_blank' rel="noreferrer">github repository</a></p>
               </div>
             </div>
           </div>
-          <div className="song-album-list-container">
-            <div className="title-bar">
-              <ProfileButton />
-            </div>
-            <div> {isLoaded && <Outlet />}</div>
+          <div className='page-bottom-container'>
+            <MusicPlayer />
           </div>
         </div>
         <Modal />
